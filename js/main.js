449d0a819a1db9657c2d94145ac1e13672c868dc
gonzaAlex <gonzaloalexramos@gmail.com>
2022-07-11 22:48:56 -0300

    comprimo 2 funciones en 1

--- js/main.js
+++ js/main.js
@@ -1,36 +1,27 @@
-// promediar nota de materia para el trimestre
-// mostrar ese promedio
-
-// variables
-let alumno = prompt("Nombre y apellido del alumno");
-let materia = prompt("¿Que materia es?");
-
-// realiza el promedio
-function promediar (nota) {
-    let promedio = parseInt(nota / 4);
-    return promedio;
-}
-
-// pregunta las notas
-function preguntarNotas () {
+function pregAndProm () {
+    //pregunto notas
     let nota1 =parseInt(prompt("Dame la primera nota"));
     let nota2 =parseInt(prompt("Dame la segunda nota"));
     let nota3 =parseInt(prompt("Dame la tercera nota"));
     let nota4 =parseInt(prompt("Dame la cuarta nota"));
     let notas = nota1 + nota2 + nota3 + nota4;
-    console.log(notas);
-    return notas;
+    // las promedio
+    let promedio = parseInt(notas / 4);
+    //llamo a la consola para saber si funciono
+    console.log(promedio);
+    // retorno el resultado
+    return promedio;
 }
-// guardo el valor de la funcion
-let promedioTotal = promediar(preguntarNotas());
 
-// bucle para registrar alumnos (objetivo a trabajar en futuras clases para guardar los datos de cada alumno o cargar sus datos y no sobreescribirlos)
 let entrada;
+// se repite hasta que el usuario no quiera ingresar mas alumnos y sus notas
 while ((entrada != "esc") && (entrada != "ESC") && (entrada != "Esc")) {
     let alumno = prompt("Nombre y apellido del alumno");
     let materia = prompt("¿Que materia es?");
-    promedioTotal = promediar(preguntarNotas());
+    // ejecuto la funcion de pedir notas y promediarlas
+    let promedioGeneral = pregAndProm();
     // salida de resultado
-    alert(`El alumno ${alumno} tiene un promedio de ${promedioTotal} en ${materia}`);
+    alert(`El alumno ${alumno} tiene un promedio de ${promedioGeneral} en ${materia}`);
+    // llamada para salir del bucle
     entrada = prompt("Escribe ESC si deseas salir");
 }
